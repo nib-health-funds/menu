e609210947e99c217a0c457fc9acd12ee320ae95
James Newell <j.newell@nib.com.au>
2015-06-03 14:24:50 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,117 @@
+# menu
+
+A menu written in pure HTML/JS so it can be styled nicely - unlike native ones.
+
+Features:
+
+ - use your own class names
+ - keyboard navigable
+
+## Installation
+
+Component:
+
+    component install nib-health-funds/menu
+    
+Browserify:
+
+    npm install --save @nib/menu
+
+## Usage
+
+HTML:
+
+    <div class="js-menu">
+        <div data-value="c#">C#</div>
+        <div data-value="go">Go</div>
+        <div data-value="js">JavaScript</div>
+        <div data-value="php">PHP</div>
+        <div data-value="ruby">Ruby</div>
+    </div>
+    
+JavaScript:
+
+    var Menu = require('menu');
+    var menu = new Menu({el: document.querySelector('.js-menu')});
+
+    menu.open();
+
+## API
+
+### Methods
+
+#### new Menu(options)
+
+Create a new menu from existing HTML.
+
+#### .open()
+
+Show the menu.
+
+#### .close()
+
+Hide the menu.
+
+#### .toggle()
+
+Toggle whether the menu is visible.
+
+#### .focus(index)
+
+Focus the menu item at the specified index.
+
+#### .focus(value)
+
+Focus the menu item with the specified value.
+ 
+#### .values() : Array
+
+Get all the menu item values.
+
+#### .labels() : Array
+
+Get all the menu item labels.
+
+### Events
+
+#### <>blurred
+
+The menu was blurred.
+
+#### <>focused
+
+The menu was focused.
+
+#### <>opening
+
+The menu is opening.
+
+#### <>opened
+
+The menu was opened.
+
+#### <>closing
+
+The menu is closing.
+
+#### <>closed
+
+The menu was closed.
+
+#### <>focus-item
+
+The user focused a menu item. How do you want to handle it?
+
+- item
+    - label
+    - value
+- index
+
+#### <>select-item
+
+The user selected a menu item. How do you want to handle it?
+
+- item
+    - label
+    - value
+- index
---
+++ index.js
@@ -0,0 +1,192 @@
+var View = require('view');
+
+module.exports = View.extend({
+
+  events: {
+    'blur':                     'emit:blurred',
+    'focus':                    'emit:focused',
+    'keydown':                  'onKeyDown',
+    'mouseenter [data-value]':  'onMouseEnter',
+    'mouseup [data-value]':     'onMouseUp'
+  },
+
+  elements: {
+    '[data-value]':             'all:items'
+  },
+
+  /**
+   * Initialise the menu
+   */
+  init: function() {
+
+    //make the menu element focusable
+    this.el.tabIndex = -1;
+
+    //set the focused menu item
+    this.focused = null;
+
+    //TODO: check items are only immediate children to allow nested menus?
+    this.items = Array.prototype.slice.call(this.items, 0);
+
+  },
+
+  /**
+   * Open the menu
+   * @returns {exports}
+   */
+  open: function() {
+    this.emit('opening');
+
+    //show the menu
+    this.el.classList.add('is-open');
+
+    //focus the menu
+    this.el.focus();
+
+    this.emit('opened');
+    return this;
+  },
+
+  /**
+   * Close the menu
+   * @returns {exports}
+   */
+  close: function() {
+    this.emit('closing');
+
+    //hide the menu
+    this.el.classList.remove('is-open');
+
+    this.emit('closed');
+    return this;
+  },
+
+  /**
+   * Toggle whether the menu is opened or closed
+   * @returns {exports}
+   */
+  toggle: function() {
+    if (this.el.classList.contains('is-open')) {
+      this.close();
+    } else {
+      this.open();
+    }
+    return this;
+  },
+
+  /**
+   * Focus a the menu item at the specified index
+   * @param   {Number|string|null} index
+   * @returns {exports}
+   */
+  focus: function(index) {
+
+    if (index === null || index === -1) {
+      this.focused = null;
+    }
+
+    for (var i=0; i<this.items.length; ++i) {
+      var item = this.items[i];
+      
+      if (typeof(index) === 'number') {
+        if (index === i) {
+          this.focused = item;
+          item.classList.add('is-focused');
+        } else {
+          item.classList.remove('is-focused');
+        }
+      } else {
+        if (index === item.getAttribute('data-value')) {
+          this.focused = item;
+          item.classList.add('is-focused');
+        } else {
+          item.classList.remove('is-focused');
+        }
+      }
+
+    }
+
+    return this
+  },
+
+  /**
+   * The item values
+   * @return  {Array.<string>}
+   */
+  values: function() {
+    return this.items.map(function(item) {
+      return item.getAttribute('data-value');
+    });
+  },
+
+  /**
+   * The item labels
+   * @return  {Array.<string>}
+   */
+  labels: function() {
+    return this.items.map(function(item) {
+      return item.innerHTML;
+    });
+  },
+
+  onMouseEnter: function(event) {
+    if (this.items.indexOf(event.target) !== -1) {
+      var item = event.target;
+      this.emit('focus-item', {label: item.innerHTML, value: item.getAttribute('data-value')}, this.items.indexOf(item));
+    }
+  },
+
+  /**
+   * Handle mouse up events
+   * @param event
+   */
+  onMouseUp: function(event) {
+    if (this.items.indexOf(event.target) !== -1) {
+
+      if (this.focused) {
+        var item = this.focused;
+        this.emit('select-item', {label: item.innerHTML, value: item.getAttribute('data-value')}, this.items.indexOf(item));
+      }
+
+    }
+  },
+
+  /**
+   * Handle key events
+   * @param event
+   */
+  onKeyDown: function(event) {
+    var key = event.which || event.keyCode;
+
+    if (key === 9 || key === 13) { //tab or enter
+      event.preventDefault(); //prevent enter from bubling up to the button or parent element
+
+      //select the focused menu item and close the menu
+      if (this.focused) {
+        var item = this.focused;
+        this.emit('select-item', {label: item.innerHTML, value: item.getAttribute('data-value')}, this.items.indexOf(item));
+      }
+
+    } else if (key === 38) { //up arrow
+
+      //focus the prev menu item
+      var i = this.items.indexOf(this.focused);
+      if (i > 0) {
+        var item = this.items[i-1];
+        this.emit('focus-item', {label: item.innerHTML, value: item.getAttribute('data-value')}, this.items.indexOf(item));
+      }
+
+    } else if (key === 40) { //down arrow
+
+      //focus the next menu item
+      var i = this.items.indexOf(this.focused);
+      if (i >= 0 && i < this.items.length-1) {
+        var item = this.items[i+1];
+        this.emit('focus-item', {label: item.innerHTML, value: item.getAttribute('data-value')}, this.items.indexOf(item));
+      }
+
+    }
+
+  }
+
+});
